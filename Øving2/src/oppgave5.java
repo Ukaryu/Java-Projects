90ced9ed5ea96a2718255973caf8ce4cce434c51
Ukaryu <shark_mario@hotmail.com>
2014-09-06 04:37:51 +0200

    New project complete

---
+++ Øving2/src/oppgave5.java
@@ -0,0 +1,58 @@
+import easyIO.*;
+
+/**
+ * Created by Martin on 06.09.2014.
+ */
+
+public class oppgave5 {
+    public static void main(String[] args) {
+        Out skjerm = new Out();
+        In tast = new In();
+
+        int sum;
+        int i;
+
+
+        skjerm.out("Vennligst skriv inn din poengsum: ");
+
+
+        for (i = 0; i < 10; i++){
+
+
+            sum = tast.inInt();
+            skjerm.outln();
+
+            if ((sum <= 100) && (sum >= 0)){
+
+                if (sum > 89){
+                    skjerm.outln("Din poengsum tilsvarer A");
+
+                } else if (90 > sum && sum > 79){
+                    skjerm.outln("Din poengsum tilsvarer B");
+
+                } else if ((80 > sum) && (sum > 59)){
+                    skjerm.outln("Din poengsum tilsvarer C");
+
+                } else if (60 > sum && sum > 49){
+                    skjerm.outln("Din poengsum tilsvarer D");
+
+                } else if (50 > sum && sum > 39){
+                    skjerm.outln("Din poengsum tilsvarer E");
+
+                } else {
+                    skjerm.outln("Din poengsum tilsvarer F, aka. stryk");
+                }
+
+                skjerm.outln();
+                skjerm.out("Vennligst skriv inn din poengsum: ");
+            } else {
+                i--;
+                skjerm.out("Poengsummen du har skrevet inn har ikke en gyldig verdi, prøv igjen: ");
+            }
+        }
+
+
+
+
+    }
+}
